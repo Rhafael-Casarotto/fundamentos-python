a8b5567ee33a76eca0b50d5ac0a3fa1aa087508b
Rhafael-Casarotto <rhafaelpcasarotto@gmail.com>
2026-01-29 23:40:40 -0300

    Relatório_de_vendas
    Projeto para estudo, e firmamento dos conceitos estudados.

---
+++ relatorio_vendas.py
@@ -0,0 +1,27 @@
+# codigo para estudo pratico
+lista_vendas = []
+meta = 5000
+for i in range(3):
+    valor = float(input(f"Digite o valor da venda {i+1}:"))
+
+lista_vendas.append(valor)
+
+total_vendas = sum(lista_vendas) 
+media = total_vendas / 3
+
+print("Relatório de vendas.")
+print(f"Total de vendas: {3}")
+print(f"Total em vendas: {total_vendas:.2f}")
+print(f"Media por vendas: {media:.2f}")
+
+if meta <= total_vendas:
+    print("Meta concluida, equipe recebe bonus!")
+else:
+    print("Meta não concluida.")
+
+
+
+
+
+
+
